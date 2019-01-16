32fd8c8ef373f72e1a1887ada3968c2cb419b498
Thomas Teo <t.teo@acfr.usyd.edu.au>
2019-01-16 14:01:15 +1100

    Master

--- sql/create_message_data_table.sql
+++ sql/create_message_data_table.sql
@@ -2,11 +2,9 @@
 
 -- DROP TABLE public.bag_message_data;
 
-CREATE SEQUENCE messageidsequence;
-
 CREATE TABLE public.bag_message_data
 (
-    messageid bigint DEFAULT NEXTVAL('messageidsequence'),
+    messageid bigserial primary key,
     positiontime timestamp without time zone,
     bagid bigint NOT NULL,
     typeid character varying(32) COLLATE pg_catalog."default" NOT NULL,
@@ -14,18 +12,10 @@ CREATE TABLE public.bag_message_data
     messagetopic character varying COLLATE pg_catalog."default" NOT NULL,
     positionmessageid bigint,
     "position" geometry,
-    CONSTRAINT bag_message_pkey PRIMARY KEY (id),
     CONSTRAINT fk_bag_id FOREIGN KEY (bagid)
         REFERENCES public.bags (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    CONSTRAINT fk_message_id FOREIGN KEY (typeid)
-        REFERENCES public.message_types (md5sum) MATCH SIMPLE
-        ON UPDATE NO ACTION
         ON DELETE CASCADE
-
-
-
 )
 WITH (
     OIDS = FALSE
